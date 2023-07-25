74f01d4a83fb710095c16de5a171c92e70dcfa15
xcnoris <augustobriel12@gmail.com>
2023-07-24 22:22:33 -0300

    Exercicios de comparação com javaScript / curso alura

---
+++ CURSO - JavaScript tipos, variáveis e funções/exercicioscomparacao.js
@@ -0,0 +1,36 @@
+a = [];
+b = 20;
+c = true;
+d = ''
+
+if (b === 20 && c != false && d == 0){
+    console.log('verdade')
+} else {
+    console.log('falso');
+}
+
+// R: verdade
+
+if (a ==0){
+    console.log('verdade');
+} else {
+    console.log('falso');
+}
+
+//R: verdade
+
+if (b == '20'){
+    console.log('verdade');
+} else {
+    console.log('falso');
+}
+
+//R:  Verdade
+
+if (d){
+    console.log('verdade');
+} else {
+    console.log('falso');
+}
+
+//R: Falso
