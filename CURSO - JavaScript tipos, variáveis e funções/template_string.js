e792a5e94858a347d3730346b93fa7487b3d914b
xcnoris <augustobriel12@gmail.com>
2023-07-24 22:00:33 -0300

    treinos com template e ternarios/ curso alura

---
+++ CURSO - JavaScript tipos, variáveis e funções/template_string.js
@@ -0,0 +1,11 @@
+var nome = 'Augusto';
+const date = new Date();
+const idade = date.getFullYear() -2006;
+
+const apresentacao = `Meu nome é ${nome}, eu tenho ${idade} anos`
+
+
+console.log(apresentacao);
+//var apresentacao = `Meu nome é ${nome}`;
+
+//
---
+++ CURSO - JavaScript tipos, variáveis e funções/ternario.js
@@ -0,0 +1,14 @@
+const idade_minima=18;
+const idade_cliente=  19;
+
+/*
+if(idade_cliente >= idade_minima) {
+    console.log('serveja');
+
+} else {
+    console.log('suco');
+}
+*/
+
+                //condição                  //true        //false
+console.log(idade_cliente >= idade_minima ? 'cerveja'  : 'suco');
