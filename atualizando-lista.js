588b2ec83623b3d98992b28733e100f275e7831b
Augusto Nascimento <134967419+xcnoris@users.noreply.github.com>
2023-08-10 12:34:31 -0300

    Atualizando um away

---
+++ atualizando-lista.js
@@ -0,0 +1,21 @@
+const nomes = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
+
+/*
+nomes.splice(1 , 2)
+nomes.push('Rodrigo') 
+
+ou pode ser escrito da seguinte forma
+*/
+nomes.splice(1, 2 , 'Rodrigo')
+                 //Adicionando um terceiro parametro ao metodo splice
+
+
+        // Outros exemplos
+console.log(nomes);
+
+animaisDoAquario = ['🐋', '🐙', '🐬', '🦈']
+
+animaisDoAquario.splice(1,0,'🐠')
+animaisDoAquario.splice(3,2,'🐟')
+
+console.log(animaisDoAquario)
