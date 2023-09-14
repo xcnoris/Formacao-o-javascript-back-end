bdbdd5393a5ae51fa78e50861dfffae255de0bc4
Augusto Nascimento <134967419+xcnoris@users.noreply.github.com>
2023-09-14 11:57:09 -0300

    Array e objetos dentro de um objetos

---
+++ CURSO - OBJETOS JS/AULA 2/alinhados.js
@@ -0,0 +1,14 @@
+const cliente = {
+    nome: "João",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["11 922233344", "22 922334455"]
+};
+cliente.endereco = {
+    rua: "R. estovaquia azul",
+    numero: "986",
+    apartamento: true,
+    complemento: "ap 745"
+}
+
+console.log(cliente);
---
+++ CURSO - OBJETOS JS/AULA 2/tipoa.js
@@ -0,0 +1,7 @@
+const cliente = {
+    nome: "João",
+    idade: 24,
+    email: "joao@firma.com",
+    telefone: ["11 922233344", "22 922334455"]
+};
+console.log(cliente.telefone[0]);
